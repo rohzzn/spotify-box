dd6fe5a22009ffe1ced867962a9eb95d5a5164f8
rohan <rohansanjeev6464@gmail.com>
2022-11-21 22:40:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ async function updateTopTracks(json) {
       gist_id,
       files: {
         [filename]: {
-          filename: '🎵 My Spotify Top Tracks',
+          filename: 'Spotify Top Tracks',
           content: lines.join('\n'),
         },
       },
